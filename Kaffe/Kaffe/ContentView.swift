557ee08c12ae60d7a8b931cdb144f323020417a3
Jens Rambeck Andersen <86779221+Jenz1000@users.noreply.github.com>
2021-07-12 10:39:20 +0200

    Det er godtaget

--- Kaffe/Kaffe/ContentView.swift
+++ Kaffe/Kaffe/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello kaffe")
-            .padding()
+        VStack{
+
+            Text("Hello kaffe")
+                .padding()
+
+        }
     }
 }
 
