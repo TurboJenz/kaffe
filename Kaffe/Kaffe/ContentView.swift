e991826e3846ec144a404239ebbc4012fcba5291
Jens Rambeck Andersen <86779221+Jenz1000@users.noreply.github.com>
2021-07-12 10:35:15 +0200

    Sådan, nu er vi igang

---
+++ Kaffe/Kaffe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kaffe
+//
+//  Created by Jens Rambeck Andersen on 12/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello kaffe")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kaffe/Kaffe/KaffeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaffeApp.swift
+//  Kaffe
+//
+//  Created by Jens Rambeck Andersen on 12/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct KaffeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
